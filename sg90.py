374b070818497a1230939e784f963e88983ac3b2
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-12-02 01:19:00 -1000

    sg90.py 서보모터 테스트 코드 추가

---
+++ sg90.py
@@ -0,0 +1,38 @@
+# 시간별로 서보모터의 팔을 움직이는 코드
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+servo_pin = 12
+
+GPIO.setup(servo_pin, GPIO.OUT) 
+servo = GPIO.PWM(servo_pin, 50)
+servo.start(0)
+
+servo_min_duty = 3
+servo_max_duty = 12
+
+def set_servo_degree(degree):
+    if degree > 180:
+        degree = 180
+    elif degree < 0:
+        degree = 0
+
+    duty = servo_min_duty+(degree*(servo_max_duty-servo_min_duty)/180.0)
+    servo.ChangeDutyCycle(duty)
+
+try:
+    while True:    
+        set_servo_degree(0)
+        sleep(1)
+        set_servo_degree(90)
+        sleep(1)
+        set_servo_degree(120)
+        sleep(1)
+        set_servo_degree(180)
+        sleep(1)
+
+finally:
+    GPIO.cleanup()
